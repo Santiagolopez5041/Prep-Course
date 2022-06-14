2e8bf7618f6b5697089e613651abcd97ca3a6b5d
Santiago Lopez Jimenez <santiagolopez5041@gmail.com>
2022-06-13 22:52:37 -0500

    Solucion de las tareas del modulo 2

---
+++ 03-JS-II/homework/Texto de homework.js
@@ -0,0 +1,7 @@
+// Los bucles for nos sirven para comprobar si una condicion resulta verdadera o falsa. Esta condición siempre va limitada por una variable y el punto en el que se debe detener. 
+// el bucle for entonces se pone a analizar cada ejecucion del bucle buscando siempre donde parar o cuando se hace falsa la condición dada
+
+//Los operadores logicos nos permiten especificar en el codigo condiciones especiales de cuando este se puede ejecutar o no.
+// por ejemplo el && hace que si las dons condiciones son verdaderas o falsas, hace que el codigo se ejecute 
+// el || hace que si alguna de las condiciones es verdadera el codigo se ejecutara sin importar la otra condicion pues cumple almenos una
+// el ! niega el statment o la condicion haciendo que se ejecute el codigo segun lo que se haya negado y ejecutando el codigo segun se haya utilizado, devolviendo siempre lo contrario
